6ae4b6a688656cac1545143f025c886c653978ae
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2024-03-11 11:49:18 +0400

    DEV-N: Removed Nested Folder

--- aznlp/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def test(request):
-    return render(request, "home/test.html")
---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from home import views
+
+
+urlpatterns = [
+    path('', views.home_page, name='home'),
+    path('about/', views.about_page, name='about'),
+]
